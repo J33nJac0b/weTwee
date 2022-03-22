01984c52c4cdbe156c43d3cd25dc00831b84f4aa
J33nJac0b <j33njacob@gmail.com>
2022-03-22 19:01:23 +0530

    First

---
+++ routes/loginRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app= express(); //instance of express
+const router = express.Router();
+  
+//type of template engine
+app.set("view engine","pug");
+app.set("views","views");
+
+router.get("/",(req,res,next)=>{
+    res.status(200).render("Login");
+})
+
+module.exports = router;
