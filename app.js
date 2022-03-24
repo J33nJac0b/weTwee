7487d1f11d292267cf949e54479e15cc20ca6c6a
J33nJac0b <j33njacob@gmail.com>
2022-03-24 15:07:56 +0530

    Login+home page layout

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 const express = require('express');
-const app= express(); //instance of express
-const port = 3003; //set port number
-const middleware= require('./middleware')
-const path =require('path')
-const bodyParser= require("body-parser");
-const mongoose= require("./database");
+const app = express();
+const port = 3003;
+const middleware = require('./middleware')
+const path = require('path')
+const bodyParser = require("body-parser");
+const mongoose = require("./database");
+const session = require("express-session");
 
 
-//create server 
-const server = app.listen(port, () => console.log("server listening on port"+port ));
-  
-//type of template engine
+const server = app.listen(port,() => console.log("Server listening" +port));
+
 app.set("view engine","pug");
 app.set("views","views");
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+    secret: "secrets",
+    resave: true,
+    saveUninitialized: false
+}))
+//routes
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,"public")));
 
 //Routes
-const loginRoute = require('./routes/loginRoutes'); 
-const registerRoute = require('./routes/registerRoutes'); 
+const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
+const logoutRoute = require('./routes/logoutRoutes');
+app.use("/login",loginRoute);
+app.use("/register",registerRoute);
+app.use("/logout",logoutRoute);
 
-app.use("/login", loginRoute);
-app.use("/register", registerRoute);
-
-
-app.get("/",middleware.requireLogin,(req,res,next)=>{
-    var payload = {
-        pageTitle: "Home"
+app.get("/",middleware.requireLogin,(req,res,next) => {
+    var payload = { 
+        pageTitle: "Home",
+        userLoggedIn: req.session.user
     }
-    res.status(200).render("home",payload); //if succesfull
-})
+
+res.status(200).render("home",payload);
+})  
--- database.js
+++ database.js
@@ -1,21 +1,17 @@
-const mongoose =require("mongoose");
-// mongoose.set('useNewUrlParser',true);
-// mongoose.set('useUnifiedTopology',true);
+const mongoose = require("mongoose");
 
 class Database{
-
-    constructor(){
-        this.connect();
-    }
-
-    connect(){
-            mongoose.connect("mongodb+srv://test:1234567890@wetweecluster.nbhlp.mongodb.net/StudentDetails?retryWrites=true&w=majority")
-            .then(()=>{
-               console.log("database connection successfull");
-            })
-             .catch((err)=>{
-               console.log("database connection error"+ err);
-            })
-    }
+  constructor(){
+    this.connect();
+  }
+  connect(){
+    mongoose.connect("mongodb+srv://test:1234567890@wetweecluster.nbhlp.mongodb.net/StudentDetails?retryWrites=true&w=majority")   .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err)=> {
+       console.log("connection not successful"+err);
+   }) 
+  }
 }
-module.exports= new Database();
+
+module.exports = new Database();
